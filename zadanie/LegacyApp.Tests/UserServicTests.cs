fd85f71a3dfee63d847b381f0ed425504e2a42ac
s28076 <haldman123@gmail.com>
2024-03-22 12:55:28 +0100

    Add Tests

---
+++ zadanie/LegacyApp.Tests/UserServicTests.cs
@@ -0,0 +1,20 @@
+namespace LegacyApp.Tests;
+
+
+public class Tests
+{
+
+
+
+   
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
